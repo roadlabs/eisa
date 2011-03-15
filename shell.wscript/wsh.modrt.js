e3f6d327eb08a2273308e9da241a1aa566d63544
infinte <infinte.cdda@hotmail.com>
2011-03-15 13:44:46 +0800

    WSH port refactorize -- TBD

---
+++ shell.wscript/wsh.modrt.js
@@ -0,0 +1,7 @@
+﻿var ESSENTIA_module = {};
+var module = ESSENTIA_module;
+"WSH module port", function(){
+	var module = ESSENTIA_module;
+	var cache = {};
+	// TBD.
+}()
--- shell.wscript/wshrt.js
+++ shell.wscript/wshrt.js
@@ -1,1 +1,13 @@
 EISA_eisa.environment = "WSH";
+var getText = function(path, encoding){
+	var fi = new ActiveXObject("ADODB.Stream");
+	fi.Type = 2;
+	fi.Mode = 3;
+	fi.Charset = encoding || "utf-8";
+	fi.Open();
+	fi.LoadFromFile(path);
+	var text = fi.readText(-1);
+	fi.Close();
+	fi = null;
+	return text.replace(/\r\n/g, '\n').replace(/\r/g, '\n');
+};
