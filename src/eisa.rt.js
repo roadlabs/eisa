28c4381f73a61523792011325aa14b34915e56c7
infinte <infinte.cdda@hotmail.com>
2011-05-10 21:53:00 +0800

    Updated documents

--- src/eisa.rt.js
+++ src/eisa.rt.js
@@ -1,9 +1,10 @@
 //: ^
 // Eisa Runtime, by Belleve Invis
 
-var EISA_eisa = function(){
+//: EISA_eisa
+var EISA_eisa = function() {
 	//: Nai
-	var Nai = function(){};
+	var Nai = function() {};
 	Nai.prototype = {
 		constructor: undefined,
 	//	toString: undefined, // comment this line for debug.
@@ -13,9 +14,9 @@ var EISA_eisa = function(){
 	};
 
 	//: derive
-	var derive = Object.craate ? Object.create : function(){
-		var F = function(){};
-		return function(obj){
+	var derive = Object.craate ? Object.create : function() {
+		var F = function() {};
+		return function(obj) {
 			F.prototype = obj;
 			return new F;
 		}
@@ -23,24 +24,24 @@ var EISA_eisa = function(){
 	var EISA_derive = derive;
 
 	//: OWNS
-	var EISA_OWNS = function(){
+	var EISA_OWNS = function() {
 		var hop = {}.hasOwnProperty;
-		return function(o,p){
+		return function(o,p) {
 			return hop.call(o,p)
 		}
 	}();
 
 	//: SLICE
-	var EISA_SLICE = function () {
+	var EISA_SLICE = function() {
 		var s = Array.prototype.slice;
-		return function (x, n) {
+		return function(x, n) {
 			return s.call(x, n);
 		};
 	} ();
 
 	//: UNIQ
-	var EISA_UNIQ = function (arr) {
-		if(!arr.length) return arr;
+	var EISA_UNIQ = function(arr) {
+		if (!arr.length) return arr;
 
 		var b = arr.slice(0).sort();
 		var t = [b[0]], tn = 1;
@@ -51,88 +52,88 @@ var EISA_eisa = function(){
 	};
 
 	//: NamedArguments
-	var NamedArguments = function(){
-		for(var i=arguments.length-2;i>=0;i-=2)
+	var NamedArguments = function() {
+		for (var i=arguments.length-2;i>=0;i-=2)
 			this[arguments[i]]=arguments[i+1];
 	};
 	var EISA_NamedArguments = NamedArguments;
 	NamedArguments.prototype = new Nai();
-	NamedArguments.fetch = function(o, p){
-		if(EISA_OWNS(o, p)) return o[p]
+	NamedArguments.fetch = function(o, p) {
+		if (EISA_OWNS(o, p)) return o[p]
 	}
-	NamedArguments.enumerate = function(o, f){	
-		for(var each in o)
-			if(EISA_OWNS(o, each))
+	NamedArguments.enumerate = function(o, f) {	
+		for (var each in o)
+			if (EISA_OWNS(o, each))
 				f.call(o[each], o[each], each);
 	}
 	NamedArguments.each = NamedArguments.enumerate;
 
 	//: EISA_CNARG
-	var EISA_CNARG = function(a){
-		if(a instanceof NamedArguments)
+	var EISA_CNARG = function(a) {
+		if (a instanceof NamedArguments)
 			return a
 		else
 			return new NamedArguments
 	}
 
 	//: AUX-METHODS
-	var EISA_M_TOP = function(){return this}();
-	var EISA_IINVOKE = function (p, s) {
+	var EISA_M_TOP = function() {return this}();
+	var EISA_IINVOKE = function(p, s) {
 		return p.item(s).apply(p, EISA_SLICE(arguments,2))
 	}
-	var EISA_RMETHOD = function (l, r, m){
+	var EISA_RMETHOD = function(l, r, m) {
 		return r[m](l)
 	}
-	var EISA_YIELDVALUE = function (a){
+	var EISA_YIELDVALUE = function(a) {
 		this.value = a[0];
 		this.values = a;
 	}
-	var EISA_RETURNVALUE = function (x){
+	var EISA_RETURNVALUE = function(x) {
 		this.value = x
 	}
-	var EISA_OBSTRUCTIVE = function(f){
+	var EISA_OBSTRUCTIVE = function(f) {
 		return {
 			build: f
 		}
 	}
 	//: OBSTRUCTIVE_SCHEMATA_M
 	var EISA_OBSTRUCTIVE_SCHEMATA_M = {
-		'return': function(t, a, v){
+		'return': function(t, a, v) {
 			return v;
 		}
 	}
 
 	//: Exceptions
-	var EISA_THROW = function(x){
+	var EISA_THROW = function(x) {
 		throw x || "[?] Unexpected error"
 	}
-	var EISA_TRY = function(f){
+	var EISA_TRY = function(f) {
 		var ret, fcatch, ffinally;
-		for(var i = arguments.length - 1; i; i--){
-			if(arguments[i] instanceof EISA_NamedArguments){
+		for (var i = arguments.length - 1; i; i--) {
+			if (arguments[i] instanceof EISA_NamedArguments) {
 				fcatch = arguments[i]['catch'];
 				ffinally = arguments[i]['finally'];
 				break;
 			}
 		};
 		
-		if(!fcatch)
-			fcatch = function(e){};
+		if (!fcatch)
+			fcatch = function(e) {};
 
 		var success = false;
 		var arg;
 
-		for(var j = 0, argn = arguments.length; j < argn; j++)
-			if(typeof (arg = arguments[j]) === "function") {
+		for (var j = 0, argn = arguments.length; j < argn; j++)
+			if (typeof (arg = arguments[j]) === "function") {
 				try {
 					ret = arg();
 					success = true
-				} catch(e){
+				} catch(e) {
 					success = false
 					fcatch(e);
 				};
-				if(success) {
-					if(ffinally) ffinally();
+				if (success) {
+					if (ffinally) ffinally();
 					return ret
 				}
 			}
@@ -140,10 +141,11 @@ var EISA_eisa = function(){
 		return ret;
 	};
 
-	var tryDefineProperty = function(){
+	//: tryDefineProperty	
+	var tryDefineProperty = function() {
 		var f;
 		try {
-			f = function(o, n, v){
+			f = function(o, n, v) {
 				Object.defineProperty(o, n, {
 					writable: false,
 					value: v,
@@ -153,52 +155,52 @@ var EISA_eisa = function(){
 				return o;
 			};
 			f({}, 'a', {});
-		} catch (e){
-			f = function(o, n, v){o[n] = v; return o};
+		} catch (e) {
+			f = function(o, n, v) {o[n] = v; return o};
 		};
 		return f;
 	}();
-	
-	tryDefineProperty(Function.prototype, 'method_', function(n, v){
+
+	tryDefineProperty(Function.prototype, 'method_', function(n, v) {
 		tryDefineProperty(this.prototype, n, v);
 	});
 
 
 	//: proto-exts
-	Object.method_('item', function (i) {
+	Object.method_('item', function(i) {
 		return this[i];
 	});
-	Object.method_('itemset', function (i, v) {
+	Object.method_('itemset', function(i, v) {
 		return this[i] = v;
 	});
-	Object.method_('compareTo',function (b) {
+	Object.method_('compareTo',function(b) {
 		return this == b ? 0 : this > b ? 1 : -1;
 	});
-	Object.method_('be',function (b) {
+	Object.method_('be',function(b) {
 		return this === b
 	});
-	Object.method_('contains',function (b) {
+	Object.method_('contains',function(b) {
 		return b in this;
 	});
-	Object.method_('of',function(v){
+	Object.method_('of',function(v) {
 		return v[this];
 	});
 
-	Function.method_('new', function () {
+	Function.method_('new', function() {
 		var obj = EISA_derive(this.prototype);
 		this.apply(obj, arguments);
 		return obj;
 	});
-	Function.method_('be',function(that){
+	Function.method_('be',function(that) {
 		return that instanceof this;
 	});
-	String.be = function(s){
+	String.be = function(s) {
 		return (typeof(s) === 'string') || s instanceof this
 	};
-	Number.be = function(s){
+	Number.be = function(s) {
 		return (typeof(s) === 'string') || s instanceof this
 	};
-	Boolean.be = function(s){
+	Boolean.be = function(s) {
 		return (typeof(s) === 'string') || s instanceof this
 	};
 
@@ -226,8 +228,8 @@ var EISA_eisa = function(){
 			return res;
 		};
 	};
-	if (!Array.prototype.some){
-		Array.prototype.some = function(fun /*, thisp */){
+	if (!Array.prototype.some) {
+		Array.prototype.some = function(fun /*, thisp */) {
 			"use strict";
 
 			if (this === void 0 || this === null)
@@ -239,7 +241,7 @@ var EISA_eisa = function(){
 				throw new TypeError();
 
 			var thisp = arguments[1];
-			for (var i = 0; i < len; i++){
+			for (var i = 0; i < len; i++) {
 				if (i in t && fun.call(thisp, t[i], i, t))
 					return true;
 			}
@@ -247,7 +249,7 @@ var EISA_eisa = function(){
 			return false;
 		};
 	}
-	if (!Array.prototype.reduce){
+	if (!Array.prototype.reduce) {
 		Array.prototype.reduce = function(fun /*, initialValue */)
 		{
 			"use strict";
@@ -280,8 +282,7 @@ var EISA_eisa = function(){
 				} while (true);
 			}
 
-			while (k < len)
-			{
+			while (k < len) {
 				if (k in t)
 					accumulator = fun.call(undefined, accumulator, t[k], k, t);
 				k++;
@@ -290,8 +291,8 @@ var EISA_eisa = function(){
 			return accumulator;
 		};
 	};
-	if (!Array.prototype.reduceRight){
-		Array.prototype.reduceRight = function(callbackfn /*, initialValue */){
+	if (!Array.prototype.reduceRight) {
+		Array.prototype.reduceRight = function(callbackfn /*, initialValue */) {
 			"use strict";
 
 			if (this === void 0 || this === null)
@@ -308,11 +309,11 @@ var EISA_eisa = function(){
 
 			var k = len - 1;
 			var accumulator;
-			if (arguments.length >= 2){
+			if (arguments.length >= 2) {
 				accumulator = arguments[1];
 			} else {
 				do {
-					if (k in this){
+					if (k in this) {
 						accumulator = this[k--];
 						break;
 					}
@@ -332,9 +333,8 @@ var EISA_eisa = function(){
 			return accumulator;
 		};
 	}
-	if (!Array.prototype.every){
-		Array.prototype.every = function(fun /*, thisp */)
-		{
+	if (!Array.prototype.every) {
+		Array.prototype.every = function(fun /*, thisp */) {
 			"use strict";
 
 			if (this === void 0 || this === null)
@@ -346,8 +346,7 @@ var EISA_eisa = function(){
 				throw new TypeError();
 
 			var thisp = arguments[1];
-			for (var i = 0; i < len; i++)
-			{
+			for (var i = 0; i < len; i++) {
 				if (i in t && !fun.call(thisp, t[i], i, t))
 					return false;
 			}
@@ -355,9 +354,8 @@ var EISA_eisa = function(){
 			return true;
 		};
 	}
-	if (!Array.prototype.filter){
-		Array.prototype.filter = function(fun /*, thisp */)
-		{
+	if (!Array.prototype.filter) {
+		Array.prototype.filter = function(fun /*, thisp */) {
 			"use strict";
 
 			if (this === void 0 || this === null)
@@ -370,10 +368,8 @@ var EISA_eisa = function(){
 
 			var res = [];
 			var thisp = arguments[1];
-			for (var i = 0; i < len; i++)
-			{
-				if (i in t)
-				{
+			for (var i = 0; i < len; i++) {
+				if (i in t) {
 					var val = t[i]; // in case fun mutates this
 					if (fun.call(thisp, val, i, t))
 						res.push(val);
@@ -383,9 +379,8 @@ var EISA_eisa = function(){
 			return res;
 		};
 	}
-	if (!Array.prototype.forEach){
-		Array.prototype.forEach = function(fun /*, thisp */)
-		{
+	if (!Array.prototype.forEach) {
+		Array.prototype.forEach = function(fun /*, thisp */) {
 			"use strict";
 
 			if (this === void 0 || this === null)
@@ -397,8 +392,7 @@ var EISA_eisa = function(){
 				throw new TypeError();
 
 			var thisp = arguments[1];
-			for (var i = 0; i < len; i++)
-			{
+			for (var i = 0; i < len; i++) {
 				if (i in t)
 					fun.call(thisp, t[i], i, t);
 			}
@@ -407,21 +401,21 @@ var EISA_eisa = function(){
 
 
 	//: Rule
-	var EISA_CREATERULE = function (l, r) {
+	var EISA_CREATERULE = function(l, r) {
 		return new Rule(l, r);
 	}
-	var Rule = function (l, r) {
+	var Rule = function(l, r) {
 		this.left = l,
 		this.right = r;
 	}
 	var EISA_Rule = Rule;
-	Rule.prototype.reverse = function () {
+	Rule.prototype.reverse = function() {
 		return new Rule(this.right, this.left);
 	}
-	Rule.prototype.toString = function () {
+	Rule.prototype.toString = function() {
 		return this.left + ' -> ' + this.right;
 	}
-	Rule.prototype.each = function (f) {
+	Rule.prototype.each = function(f) {
 		if (typeof this.left === 'number' && typeof this.right === 'number') {
 			if (this.left <= this.right) {
 				for (var i = this.left; i <= this.right; i++) {
@@ -440,7 +434,7 @@ var EISA_eisa = function(){
 	var eisa = {};
 	eisa.version = 'hoejuu';
 
-	eisa.log = function(message){};
+	eisa.log = function(message) {};
 
 	eisa.runtime = eisa.rt = {
 		CNARG: EISA_CNARG,
@@ -470,35 +464,35 @@ var EISA_eisa = function(){
 var EISA_DEBUGTIME = false;
 
 //: eisa-module-helpers
-"Eisa module helpers", function(eisa){
+"Eisa module helpers", function(eisa) {
 	var module = NECESSARIA_module;
 	var OWNS = EISA_eisa.rt.OWNS;
-	eisa.using = function(libs, f){
+	eisa.using = function(libs, f) {
 		var importings = [], immediates = [];
-		for(var i = 0; i < libs.length; i++){
-			if(typeof libs[i] === 'string')
+		for (var i = 0; i < libs.length; i++) {
+			if (typeof libs[i] === 'string')
 				importings.push(libs[i]);
 			else
 				immediates.push(libs[i]);
 		};
-		module.provide(importings, function(require){
+		module.provide(importings, function(require) {
 			var vals = {}, obts = {}, YES = {};
-			for(var i = 0; i < importings.length; i++)
-				require.enumerate(libs[i], function(n, v){
+			for (var i = 0; i < importings.length; i++)
+				require.enumerate(libs[i], function(n, v) {
 					vals[n] = v;
 					obts[n] = YES
 				});
-			for(var i = 0; i < immediates.length; i++){
+			for (var i = 0; i < immediates.length; i++) {
 				var immlib = immediates[i];
-				for(var each in immlib)
-					if(OWNS(immlib, each)){
+				for (var each in immlib)
+					if (OWNS(immlib, each)) {
 						vals[each] = immlib[each];
 						obts[each] = YES
 					};
 			};
-			var enumVars = function(f){
-				for(var each in vals)
-					if(obts[each] === YES)
+			var enumVars = function(f) {
+				for (var each in vals)
+					if (obts[each] === YES)
 						f(vals[each], each)
 			};
 			return f.call(vals, vals, enumVars);
@@ -507,30 +501,30 @@ var EISA_DEBUGTIME = false;
 }(EISA_eisa);
 
 //: eisa-script
-"Eisa Script", function(eisa){
+"Eisa Script", function(eisa) {
 	eisa.languages = {};
 	eisa.Script = function(source, language, config, libraries, callback) {
 
 		var libs = ['stl', 'mod'].concat(libraries || []);
 
-		eisa.using(libs, function(initvs, inita){
+		eisa.using(libs, function(initvs, inita) {
 			var ast = language.parse(language.lex(source), source, inita);
 			var lfcr;
 
 			return callback({
-				compile: function(){
+				compile: function() {
 					lfcr = language.Compiler(ast, config).compile(); 
 					return lfcr;
 				},
-				asyncCompile: function(onSuccess, onStep){
+				asyncCompile: function(onSuccess, onStep) {
 					language.Compiler(ast, config).asyncCompile(
-						function(cm){
+						function(cm) {
 							lfcr = cm;
 							onSuccess.apply(this, arguments)
 						}, onStep);
 				},
-				start: function(){
-					if(!lfcr) this.compile();
+				start: function() {
+					if (!lfcr) this.compile();
 					lfcr.wrappedF.apply(initvs, arguments);
 				}
 			})
